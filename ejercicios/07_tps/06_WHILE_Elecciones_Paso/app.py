ee0d98073ce4a12aa9a547479fc6ffcb2a220f74
Candela <85591970+cande-00@users.noreply.github.com>
2023-07-24 20:21:09 -0300

    tp while validaciones terminado

--- ejercicios/07_tps/06_WHILE_Elecciones_Paso/app.py
+++ ejercicios/07_tps/06_WHILE_Elecciones_Paso/app.py
@@ -29,7 +29,55 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        total_de_votos = 0
+        suma_edad = 0
+        max_votos = 0
+        min_votos = 0
+        max_nombre_votos = ""
+        min_nombre_votos = ""
+        min_edad_votos = 0
+        cant_edades = 0
+        promedio_edades = 0
+        
+
+        while True:
+            nombre = prompt("Candidato", "Ingrese el nombre")
+            while nombre == None or nombre == "" or not nombre.isalpha():
+                nombre = prompt("Candidato", "Reingrese el nombre")
+            
+            edad = prompt("Candidato", "Ingrese la edad")
+            if edad == None or not edad.isdigit() or int(edad) < 25 or int(edad) > 90:
+                edad = prompt("Candidato", "Reingrese la edad")
+            else:
+                suma_edad += int(edad)
+                cant_edades += 1
+            edad = int(edad)
+
+            cant_de_votos = prompt("Candidato", "Ingrese cant de votos")
+            while cant_de_votos == None or not cant_de_votos.isdigit() or int(cant_de_votos) < 0:
+                cant_de_votos = prompt("Candidato", "Reingrese cant de votos")
+            cant_de_votos = int(cant_de_votos)
+
+            if max_votos == None or cant_de_votos > max_votos:
+                max_votos = cant_de_votos
+                max_nombre_votos = nombre
+
+            if min_votos == None or cant_de_votos < min_votos:
+                min_votos = cant_de_votos
+                min_nombre_votos = nombre
+                min_edad_votos = edad
+
+            total_de_votos += cant_de_votos
+            promedio_edades = suma_edad / cant_edades
+
+            print(f"El candidato mas votado es: {max_nombre_votos}" )
+            print(f"El candidato menos votado es: {min_nombre_votos} y su edad es: {min_edad_votos}")
+            print(f"El promedio de las edades es: {promedio_edades}")
+            print(f"La cantidad de votos es: {total_de_votos}")
+
+            continuar = question("Candidato", "¿Desea continuar?")
+            if not continuar:
+                break
 
 
 if __name__ == "__main__":
